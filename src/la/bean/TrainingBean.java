d52299c6031b505dcc14a5539d612cc79f562ee5
Ha Jaehyeong <jaehyeong.ha@bizreach.co.jp>
2019-05-24 09:36:16 +0900

    update Bean files

--- src/la/bean/TrainingBean.java
+++ src/la/bean/TrainingBean.java
@@ -1,4 +1,62 @@
 package la.bean;
 
+import java.util.Date;
+
 public class TrainingBean {
+    private int trainingId;
+    private String trainingName;
+    private int traineeId;
+    private int muscleCategoryId;
+    private int areaId;
+    private Date date;
+    private Enum type;
+
+    TrainingBean(int trainingId, String trainingName, int traineeId, int muscleCategoryId,
+                    int areaId, Date date, Enum type) {
+        this.trainingId = trainingId;
+        this.trainingName = trainingName;
+        this.traineeId = traineeId;
+        this.muscleCategoryId = muscleCategoryId;
+        this.areaId = areaId;
+        this.date = date;
+        this.type = type;
+    }
+
+    void setTrainingName(String name) {
+        this.trainingName = name;
+    }
+    void setTraineeId(int traineeId) {
+        this.traineeId = traineeId;
+    }
+    void setMuscleCategoryId(int muscleCategoryId) {
+        this.muscleCategoryId = muscleCategoryId;
+    }
+    void setAreaId(int areaId) {
+        this.areaId = areaId;
+    }
+    void setDate(Date date) {
+        this.date = date;
+    }
+    void setType(Enum type) {
+        this.type = type;
+    }
+    int getTrainingId() {
+        return this.trainingId;
+    }
+    int getTraineeId() {
+        return this.traineeId;
+    }
+    int getMuscleCategoryId() {
+        return this.muscleCategoryId;
+    }
+    int getAreaId() {
+        return this.areaId;
+    }
+    Date getDate() {
+        return this.date;
+    }
+    Enum getType() {
+        return this.type;
+    }
+
 }
