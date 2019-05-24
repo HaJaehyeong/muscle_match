96652ea621cee04f2c2e66b2ce77f116b42da1d3
enket <tr.nayama@gmail.com>
2019-05-24 15:32:04 +0900

    forward to original page

--- src/la/servlet/TrainingController.java
+++ src/la/servlet/TrainingController.java
@@ -3,10 +3,13 @@ package la.servlet;
 import la.DAO.DAOException;
 import la.DAO.TrainingDAO;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("/trainingController")
 public class TrainingController extends HttpServlet {
@@ -17,17 +20,22 @@ public class TrainingController extends HttpServlet {
         int training_id = Integer.parseInt(request.getParameter("training_id"));
         int trainee_id = Integer.parseInt(request.getParameter("trainee_id"));
 
+        request.getHeader("Referer");
+        RequestDispatcher rd = request.getRequestDispatcher("/trainingList.jsp");
+
         try {
             TrainingDAO dao = new TrainingDAO();
             switch (state) {
                 case "join":
                     dao.joinTraining(training_id, trainee_id);
+                    rd.forward(request, response);
                     break;
                 case "cancel":
                     dao.cancelTraining(training_id, trainee_id);
+                    rd.forward(request, response);
                     break;
             }
-        } catch (DAOException e) {
+        } catch (DAOException | ServletException | IOException e) {
             e.printStackTrace();
         }
 
