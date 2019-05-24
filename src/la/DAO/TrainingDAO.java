f557d2b0c981c211878cefa75939dc5ca7f2707d
tomoya-kinsho <tomoya.kinsho@bizreach.co.jp>
2019-05-24 10:21:36 +0900

    findAllTrainingメソッドを追記

--- src/la/DAO/TrainingDAO.java
+++ src/la/DAO/TrainingDAO.java
@@ -1,6 +1,15 @@
 package la.DAO;
-
 import DbManager;
+import la.bean.TrainingBean;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 
 public class TrainingDAO {
     private Connection con;
@@ -12,5 +21,54 @@ public class TrainingDAO {
     public TrainingDAO() throws DAOException {
         db = new DbManager();
         con = db.getConnection(con);
+    }
+
+    public List<TrainingBean> findAllTraining() throws DAOException{
+        // 全てのトレーニングを取得する
+        if(con == null) {
+            con = db.getConnection(con);
+        }
+
+        try {
+            // training_tableとjoin_taraining tebleを外部結合することで、検索したユーザーがどのトレーニングに参加しているのかを
+            // 判断する stateカラムを取得することができる
+            // state(join, cancel) stateの値がない場合は、nullを返す（仕方なくnullを返すようにしている。）
+            String sql = "select *, jt.state" +
+                    "from training as tr left outer join join_training as jt" +
+                    "on tr.taining.id = jt.training_id" +
+                    "&& tr.tainee.id = jt.trainee_id";
+            st = con.prepareStatement(sql);
+            rs = st.executeQuery();
+            List<TrainingBean> list = new ArrayList<>();
+            while (rs.next()) {
+                createTrainingBeanList(list, rs);
+            }
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new DAOException("レコードの取得に失敗しました。");
+        } finally {
+            db.closeResources(st, rs, con);
+        }
+    }
+
+    private void createTrainingBeanList(List<TrainingBean> list, ResultSet rs) throws DAOException{
+        try {
+            int trainingId = rs.getInt("TRAINING_ID");
+            int muscleCategoryId = rs.getInt("MUSCLE_CATEGORY_ID");
+            int areaId = rs.getInt("MUSCLE_CATEGORY_ID");
+            String trainingName = rs.getString("TRAINING_NAME");
+            Date date = rs.getDate("DATE");
+            // active or inactiveを取得
+            String type = rs.getString("TYPE");
+            int traineeId = rs.getInt("TRAINEE_ID");
+            // join or cancel or null(参加・キャンセルもしていないトレーニング)
+            String state = rs.getString("STATE");
+            TrainingBean bean = new TrainingBean(trainingId, trainingName, traineeId, muscleCategoryId, areaId, date, type, state);
+            list.add(bean);
+        } catch (Exception e) {
+            throw new DAOException("データベースの操作に失敗しました。");
+        }
+
     }
 }
