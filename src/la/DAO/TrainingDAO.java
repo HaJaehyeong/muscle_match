febfebe9561102a5ac682e60b68a16dc76405568
tomoya-kinsho <tomoya.kinsho@bizreach.co.jp>
2019-05-24 09:59:47 +0900

    add field

--- src/la/DAO/TrainingDAO.java
+++ src/la/DAO/TrainingDAO.java
@@ -1,4 +1,16 @@
 package la.DAO;
 
+import DbManager;
+
 public class TrainingDAO {
+    private Connection con;
+    private DbManager db;
+    private String sql = null;
+    private PreparedStatement st = null;
+    private ResultSet rs = null;
+
+    public TrainingDAO() throws DAOException {
+        db = new DbManager();
+        con = db.getConnection(con);
+    }
 }
